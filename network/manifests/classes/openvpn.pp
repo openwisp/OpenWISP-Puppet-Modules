a5ac0176bc00fb6e7eae461aa8a2f9df6520fc6e
Matteo Latini <mtylty@gmail.com>
2011-10-07 09:50:42 +0200

    adds openvpn class to network module

---
+++ network/manifests/classes/openvpn.pp
@@ -0,0 +1,19 @@
+class openvpn {
+  package { "openvpn": ensure => installed }
+
+  service { "openvpn":
+    enable => true,
+    ensure => running,
+    require => Package["openvpn"]
+  }
+
+  file { "/etc/openvpn":
+    source => [ "puppet:///files/network/${fqdn}/openvpn",
+                "puppet:///files/network/${operatingsystem}/${lsbdistcodename}/openvpn",
+                "puppet:///files/network/${operatingsystem}/openvpn",
+                "puppet:///files/network/openvpn" ],
+    require => Package["openvpn"],
+    notify => Service["openvpn"],
+    mode => 0400, owner => root, group => root;
+  }
+}
