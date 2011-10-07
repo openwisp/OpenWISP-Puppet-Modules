5ef9dfc97995f09f5129fc280819b13c6041801b
Matteo Latini <mtylty@gmail.com>
2011-10-07 11:15:36 +0200

    adds rsyncd module

---
+++ rsyncd/manifests/classes/base.pp
@@ -0,0 +1,36 @@
+class rsyncd {
+  package { "rsync": ensure => installed }
+
+  service { "rsync":
+    enable => true,
+    ensure => running,
+    require => [ Package["rsync"], Exec["enable-rsync"] ]
+  }
+
+  exec { "enable-rsync": 
+    command => "sed -ie 's/RSYNC_ENABLE=false/RSYNC_ENABLE=true/' /etc/default/rsync",
+    refreshonly => true
+  } 
+
+  file { "/etc/rsyncd.conf":
+    ensure => file,
+    source => [ "puppet:///files/rsyncd/${fqdn}/rsyncd.conf",
+                "puppet:///files/rsyncd/${operatingsystem}/${lsbdistcodename}/rsyncd.conf",
+                "puppet:///files/rsyncd/${operatingsystem}/rsyncd.conf",
+                "puppet:///files/rsyncd/rsyncd.conf" ],
+    require => Package["rsync"],
+    notify => Service["rsync"],
+    mode => 0644, owner => root, group => root;
+  }
+
+  file { "/etc/rsyncd.secrets":
+    ensure => file,
+    source => [ "puppet:///files/rsyncd/${fqdn}/rsyncd.secrets",
+                "puppet:///files/rsyncd/${operatingsystem}/${lsbdistcodename}/rsyncd.secrets",
+                "puppet:///files/rsyncd/${operatingsystem}/rsyncd.secrets",
+                "puppet:///files/rsyncd/rsyncd.secrets" ],
+    require => Package["rsync"],
+    notify => Service["rsync"],
+    mode => 0600, owner => root, group => root;
+  }
+}
