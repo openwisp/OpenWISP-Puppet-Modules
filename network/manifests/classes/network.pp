c3b78845a4692c51d74bcb96f4da96bcddf6e1fc
Matteo Latini <mtylty@gmail.com>
2011-10-07 14:48:51 +0200

    adds resolv conf class

--- network/manifests/classes/network.pp
+++ network/manifests/classes/network.pp
@@ -1,18 +1,28 @@
 class network {
   package { "bridge-utils": ensure => installed }
-  package { "resolvconf": ensure => installed }
   package { "vlan": ensure => installed }
 
   file { "/etc/network/interfaces":
     source => [ "puppet:///files/network/${fqdn}/interfaces" ],
-    require => [ Package["bridge-utils"], Package["resolvconf"], Package["vlan"] ],
+    require => [ Package["bridge-utils"], Package["vlan"] ],
     notify => Exec['restart-ifaces'],
-    mode => 0400, owner => root, group => root;
+    mode => 0644, owner => root, group => root;
+  }
+
+  file { "/etc/resolv.conf":
+    source => [ "puppet:///files/network/${fqdn}/resolv.conf",
+                "puppet:///files/network/${operatingsystem}/${lsbdistcodename}/resolv.conf",
+                "puppet:///files/network/${operatingsystem}/resolv.conf",
+                "puppet:///files/network/resolv.conf",
+                "puppet:///modules/network/${operatingsystem}/${lsbdistcodename}/resolv.conf",
+                "puppet:///modules/network/${operatingsystem}/resolv.conf",
+                "puppet:///modules/network/resolv.conf" ],
+    mode => 0644, owner => root, group => root;
   }
 
   exec { "restart-ifaces":
     command => "/etc/init.d/networking restart",
-    require => [ Package["bridge-utils"], Package["resolvconf"], Package["vlan"] ],
+    require => [ Package["bridge-utils"], Package["vlan"] ],
     refreshonly => true;
   }
 }
