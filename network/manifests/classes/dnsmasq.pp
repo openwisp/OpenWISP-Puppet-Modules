e4f72897df1a0814d7db70c90baa58710e773ad9
Matteo Latini <mtylty@gmail.com>
2011-10-07 15:49:43 +0200

    adds zabbix_agent and dnsmasq class to network module

---
+++ network/manifests/classes/dnsmasq.pp
@@ -0,0 +1,24 @@
+class dnsmasq {
+  package { "dnsmasq": ensure => installed }
+
+  service { "dnsmasq":
+    enable => true,
+    ensure => running,
+    require => [ Package["dnsmasq"], Exec["enable-dnsmasq"] ]
+  }
+
+  exec { "enable-dnsmasq": 
+    command => "sed -ie 's/ENABLE=false/ENABLE=true/' /etc/default/dnsmasq",
+    refreshonly => true
+  } 
+
+  file { "/etc/dnsmasq.d/":
+    source => [ "puppet:///files/network/${fqdn}/dnsmasq",
+                "puppet:///files/network/${operatingsystem}/${lsbdistcodename}/dnsmasq",
+                "puppet:///files/network/${operatingsystem}/dnsmasq",
+                "puppet:///files/network/dnsmasq" ],
+    require => Package["dnsmasq"],
+    notify => Service["dnsmasq"],
+    mode => 0644, owner => root, group => root;
+  }
+}
---
+++ network/manifests/classes/zabbix_agent.pp
@@ -0,0 +1,19 @@
+class zabbix_agent {
+  package { "zabbix-agent": ensure => installed }
+
+  service { "zabbix-agent":
+    enable => true,
+    ensure => running,
+    require => Package["zabbix-agent"]
+  }
+
+  file { "/etc/zabbix/zabbix_agentd.conf":
+    source => [ "puppet:///files/network/${fqdn}/zabbix_agentd.conf",
+                "puppet:///files/network/${operatingsystem}/${lsbdistcodename}/zabbix_agentd.conf",
+                "puppet:///files/network/${operatingsystem}/zabbix_agentd.conf",
+                "puppet:///files/network/zabbix_agentd.conf" ],
+    require => Package["zabbix-agent"],
+    notify => Service["zabbix-agent"],
+    mode => 0640, owner => root, group => root;
+  }
+}
