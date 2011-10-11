8307253c99dd0d51488779383622bbf81178ada9
Matteo Latini <mtylty@gmail.com>
2011-10-11 11:43:19 +0200

    adds specific file/link management for radius

--- radius/manifests/classes/base.pp
+++ radius/manifests/classes/base.pp
@@ -15,7 +15,50 @@ class radius {
                 "puppet:///files/radius/freeradius" ],
     require => Package["freeradius"],
     notify => Service["freeradius"],
-    recurse => true,
-    mode => 06400, owner => root;
+    recurse => true, ignore => 'certs',
+    mode => 0640, owner => freerad, group => freerad;
+  }
+
+  file { "/etc/freeradius/certs":
+    ensure => directory,
+    mode => 0640, owner => freerad, group => freerad;
+  }
+
+  file { "/etc/freeradius/certs/dh":
+    ensure => file,
+    source => [ "puppet:///files/radius/${fqdn}/freeradius/certs/dh",
+                "puppet:///files/radius/${operatingsystem}/${lsbdistcodename}/freeradius/certs/dh",
+                "puppet:///files/radius/${operatingsystem}/freeradius/certs/dh",
+                "puppet:///files/radius/freeradius/certs/dh" ],
+    require => File['/etc/freeradius/certs'],
+    mode => 0640, owner => freerad, group => freerad;
+  }
+
+  file { "/etc/freeradius/certs/ca.pem":
+    ensure => link,
+    target => '/etc/ssl/certs/ca.pem',
+    require => File['/etc/freeradius/certs'],
+    mode => 0640, owner => freerad, group => freerad;
+  }
+
+  file { "/etc/freeradius/certs/random":
+    ensure => link,
+    target => '/dev/urandom',
+    require => File['/etc/freeradius/certs'],
+    mode => 0640, owner => freerad, group => freerad;
+  }
+
+  file { "/etc/freeradius/certs/server.key":
+    ensure => link,
+    target => '/etc/ssl/private/ssl-cert-snakeoil.key',
+    require => File['/etc/freeradius/certs'],
+    mode => 0640, owner => freerad, group => freerad;
+  }
+
+  file { "/etc/freeradius/certs/server.pem":
+    ensure => link,
+    target => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+    require => File['/etc/freeradius/certs'],
+    mode => 0640, owner => freerad, group => freerad;
   }
 }
