96cd51f1d9be87bdd6900ef00a325dde8880ba00
Matteo Latini <mtylty@gmail.com>
2011-10-11 15:58:17 +0200

    adds config dir to shared

--- rails/manifests/classes/rails.pp
+++ rails/manifests/classes/rails.pp
@@ -1,5 +1,5 @@
 class rails($path, $adapter, $db, $pool_size, $db_user, $db_password) {
-  file { "${$path}/shared/database.yml":
+  file { "${$path}/shared/config/database.yml":
     ensure => file, recurse => true,
     content => template('rails/database.yml.erb'),
     mode => 0640, owner => root, group => root;
