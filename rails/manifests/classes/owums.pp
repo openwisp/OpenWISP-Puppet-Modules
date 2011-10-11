0bf9f73b7e34f47f62baebb979e8e25c1b426932
Matteo Latini <mtylty@gmail.com>
2011-10-11 16:12:59 +0200

    adds require packages for owums

--- rails/manifests/classes/owums.pp
+++ rails/manifests/classes/owums.pp
@@ -1,5 +1,10 @@
 class owums($path = '/var/rails', $db_password, $pool_size = '10') {
   package { "libmagickwand-dev": ensure => installed }
+  package { "lame": ensure => installed }
+  package { "festival": ensure => installed }
+  package { "festvox-italp16k": ensure => installed }
+  package { "festvox-rablpc16k": ensure => installed }
+  package { "librsvg2-bin": ensure => installed }
 
   class { rails:
       path => "${$path}/${$name}",
