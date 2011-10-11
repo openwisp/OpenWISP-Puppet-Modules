fb6c28de255e6c25e3b5962720220159860af980
Matteo Latini <mtylty@gmail.com>
2011-10-11 16:03:07 +0200

    adds directory for conf

--- rails/manifests/classes/rails.pp
+++ rails/manifests/classes/rails.pp
@@ -1,6 +1,12 @@
 class rails($path, $adapter, $db, $pool_size, $db_user, $db_password) {
+  file { "${$path}/shared/config":
+    ensure => directory, recurse => true,
+    content => template('rails/database.yml.erb'),
+    mode => 0640, owner => root, group => root;
+  }
+
   file { "${$path}/shared/config/database.yml":
-    ensure => file, recurse => true,
+    ensure => file,
     content => template('rails/database.yml.erb'),
     mode => 0640, owner => root, group => root;
   }
