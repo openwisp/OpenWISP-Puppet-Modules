f4a726abc58b8422d67bca3f240069729aa474e1
Matteo Latini <mtylty@gmail.com>
2011-10-11 16:33:49 +0200

    adds drop if db present

--- mysql/manifests/classes/db.pp
+++ mysql/manifests/classes/db.pp
@@ -1,7 +1,7 @@
 class mysql::db($db_name, $user, $password) {
   exec { "create-${db_name}-db":
-    unless => "/usr/bin/mysql -uroot -p$mysql_password -e \"use ${db_name};\"",
-    command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database ${db_name}; grant all on ${db_name}.* to ${user}@localhost identified by '$password';\"",
+    unless => "/usr/bin/mysql -u$user -p$password -e \"use ${db_name};\"",
+    command => "/usr/bin/mysql -uroot -p$mysql_password -e \"drop database if exists ${db_name}; create database ${db_name}; grant all on ${db_name}.* to ${user}@localhost identified by '$password';\"",
     require => Service["mysql"]
   }
 }
