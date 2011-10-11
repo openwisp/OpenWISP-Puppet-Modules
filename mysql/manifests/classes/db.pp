47602b7bfed481249fd03edcfd06f302175234a6
Matteo Latini <mtylty@gmail.com>
2011-10-11 15:54:53 +0200

    fixes stuff in mysql module

--- mysql/manifests/classes/db.pp
+++ mysql/manifests/classes/db.pp
@@ -1,6 +1,6 @@
 class mysql::db($db_name, $user, $password) {
   exec { "create-${db_name}-db":
-    unless => "/usr/bin/mysql -u${user} -p${password} ${db_name}",
+    unless => "/usr/bin/mysql -uroot -p$mysql_password -e \"use ${db_name};\"",
     command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database ${db_name}; grant all on ${db_name}.* to ${user}@localhost identified by '$password';\"",
     require => Service["mysql"]
   }
