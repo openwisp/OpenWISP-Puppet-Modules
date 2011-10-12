c49c30330ae935bc456375d99e71d3fc38a44662
Matteo Latini <mtylty@gmail.com>
2011-10-12 12:18:07 +0200

    changes to apache2 module

--- rails/manifests/classes/owums.pp
+++ rails/manifests/classes/owums.pp
@@ -1,4 +1,4 @@
-class owums($path = '/var/rails', $db_password, $pool_size = '10') {
+class owums($path = '/var/www', $db_password, $pool_size = '10') {
   package { "libmagickwand-dev": ensure => installed }
   package { "lame": ensure => installed }
   package { "festival": ensure => installed }
@@ -7,7 +7,7 @@ class owums($path = '/var/rails', $db_password, $pool_size = '10') {
   package { "librsvg2-bin": ensure => installed }
 
   class { rails:
-      path => "${$path}/${$name}",
+      path => "${path}/${name}",
       adapter => 'mysql',
       db => $name,
       pool_size => $pool_size,
--- rails/manifests/classes/rails.pp
+++ rails/manifests/classes/rails.pp
@@ -1,11 +1,11 @@
 class rails($path, $adapter, $db, $pool_size, $db_user, $db_password) {
-  file { "${$path}/shared/config":
+  file { [ "${path}", "${path}/shared", "${path}/shared/config" ]:
     ensure => directory, recurse => true,
     content => template('rails/database.yml.erb'),
     mode => 0640, owner => root, group => root;
   }
 
-  file { "${$path}/shared/config/database.yml":
+  file { "${path}/shared/config/database.yml":
     ensure => file,
     content => template('rails/database.yml.erb'),
     mode => 0640, owner => root, group => root;
