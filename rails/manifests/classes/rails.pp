73e9f343bc4bed4a000139a06086141e829960b7
Matteo Latini <mtylty@gmail.com>
2011-10-12 15:41:08 +0200

    fixes bugs

--- rails/manifests/classes/rails.pp
+++ rails/manifests/classes/rails.pp
@@ -1,14 +1,13 @@
 class rails($path, $adapter, $db, $pool_size, $db_user, $db_password) {
   file { [ "${path}", "${path}/shared", "${path}/shared/config" ]:
     ensure => directory, recurse => true,
-    content => template('rails/database.yml.erb'),
-    mode => 0640, owner => root, group => root;
+    mode => 0644, owner => root, group => root;
   }
 
   file { "${path}/shared/config/database.yml":
     ensure => file,
     content => template('rails/database.yml.erb'),
-    mode => 0640, owner => root, group => root;
+    mode => 0644, owner => root, group => root;
   }
 
   class { mysql::db: db_name => $db, user => $db_user, password => $db_password }
