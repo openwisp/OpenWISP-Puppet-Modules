2a226c961860b076bc0155d96881e5f504064595
Matteo Latini <mtylty@gmail.com>
2011-10-17 11:38:12 +0200

    more forgiving db creation

--- mysql/manifests/classes/db.pp
+++ mysql/manifests/classes/db.pp
@@ -1,7 +1,7 @@
 class mysql::db($db_name, $user, $password) {
   exec { "create-${db_name}-db":
     unless => "/usr/bin/mysql -u$user -p$password -e \"use ${db_name};\"",
-    command => "/usr/bin/mysql -uroot -p$mysql_password -e \"drop database if exists ${db_name}; create database ${db_name}; grant all on ${db_name}.* to ${user}@localhost identified by '$password';\"",
+    command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database if not exists ${db_name}; grant all on ${db_name}.* to ${user}@localhost identified by '$password';\"",
     require => Service["mysql"]
   }
 }
