148f6027b58679544d5e1b8e7a4c7d299b328616
Matteo Latini <mtylty@gmail.com>
2011-10-17 15:32:49 +0200

    adds other rails application

---
+++ rails/manifests/classes/owcpm.pp
@@ -0,0 +1,24 @@
+class owcpm($path = '/var/rails', $db_password, $pool_size = '10') {
+  class { rails:
+      app_name => $name,
+      path => $path,
+      adapter => 'mysql',
+      db => $name,
+      pool_size => $pool_size,
+      db_user => $name,
+      db_password => $db_password
+  }
+
+  file { "init_script":
+    path =>"/etc/init.d/${name}-daemons",
+    ensure => file, 
+    content => template('rails/init_script.erb'),
+    mode => 0751, owner => root, group => root;
+  }
+
+  service { "${name}-daemons":
+    enable => true,
+    ensure => running,
+    require => [ File["init_script"] ]
+  }
+}
