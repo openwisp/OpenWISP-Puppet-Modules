50d2e4a556d6a120bfa8bbcf1c87137cd65951bf
Matteo Latini <mtylty@gmail.com>
2011-10-17 11:29:56 +0200

    adds rails startup script

--- rails/manifests/classes/owums.pp
+++ rails/manifests/classes/owums.pp
@@ -1,10 +1,8 @@
 class owums($path = '/var/rails', $db_password, $pool_size = '10') {
-  package { "libmagickwand-dev": ensure => installed }
-  package { "lame": ensure => installed }
-  package { "festival": ensure => installed }
-  package { "festvox-italp16k": ensure => installed }
-  package { "festvox-rablpc16k": ensure => installed }
-  package { "librsvg2-bin": ensure => installed }
+  package { "dependencies":
+    name => ["libmagickwand-dev", "lame", "festival", "festvox-italp16k", "festvox-rablpc16k", "librsvg2-bin"],
+    ensure => installed
+  }
 
   class { rails:
       app_name => $name,
@@ -14,5 +12,18 @@ class owums($path = '/var/rails', $db_password, $pool_size = '10') {
       pool_size => $pool_size,
       db_user => $name,
       db_password => $db_password
+  }
+
+  file { "init_script":
+    path =>"/etc/init.d/${name}-daemons",
+    ensure => file, 
+    content => template('rails/init_script.erb'),
+    mode => 0751, owner => root, group => root;
+  }
+
+  service { "${name}-daemons":
+    enable => true,
+    ensure => running,
+    require => [ Package["dependencies"], File["init_script"] ]
   }
 }
