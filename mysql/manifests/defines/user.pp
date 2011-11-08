3cf6c66cae37b38a5367c04860bb9cc5437fb1d5
Matteo Latini <mtylty@gmail.com>
2011-11-08 15:26:55 +0100

    oops, fixes mysql user

--- mysql/manifests/defines/user.pp
+++ mysql/manifests/defines/user.pp
@@ -1,7 +1,7 @@
-define mysql::user($user, $password, $grant) {
-  exec { "create-${user}":
-    unless => "/usr/bin/mysql -u${user} -p${password}",
-    command => "/usr/bin/mysql -uroot -p${mysql_password} -e \"grant ${grant} to ${user}@localhost identified by '${password}';\"",
+define mysql::user($password, $grant) {
+  exec { "create-user-${name}":
+    unless => "/usr/bin/mysql -u${name} -p${password}",
+    command => "/usr/bin/mysql -uroot -p${mysql_password} -e \"grant ${grant} to ${name}@localhost identified by '${password}';\"",
     require => Service["mysql"]
   }
 }
