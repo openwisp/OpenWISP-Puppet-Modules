e109704ef2050e51ec001414637cb26ca89e6efa
Matteo Latini <mtylty@gmail.com>
2011-11-10 11:06:23 +0100

    fixes mysql user define

--- mysql/manifests/defines/user.pp
+++ mysql/manifests/defines/user.pp
@@ -1,7 +1,13 @@
 define mysql::user($password, $grant) {
   exec { "create-user-${name}":
-    unless => "/usr/bin/mysql -u${name} -p${password}",
+    unless => "mysql -u${name} -p${password} && test -f ~/.puppet_mysql_${name}_grants && grep -F \"${grant}\" ~/.puppet_mysql_${name}_grants",
     command => "/usr/bin/mysql -uroot -p${mysql_password} -e \"grant ${grant} to ${name}@localhost identified by '${password}';\"",
-    require => Service["mysql"]
+    require => Service["mysql"],
+    notify => Exec["mysql-${name}-grants"]
+  }
+
+  exec { "mysql-${name}-grants":
+    command => "echo \"${grant}\" > ~/.puppet_mysql_${name}_grants",
+    refreshonly => true
   }
 }
