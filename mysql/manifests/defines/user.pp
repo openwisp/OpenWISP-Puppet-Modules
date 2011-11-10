d2633d4175b485db4789dc29b05dcdd3d0077cff
Matteo Latini <mtylty@gmail.com>
2011-11-10 15:05:36 +0100

    fixes mysql module

--- mysql/manifests/defines/user.pp
+++ mysql/manifests/defines/user.pp
@@ -1,13 +1,27 @@
-define mysql::user($password, $grant) {
-  exec { "create-user-${name}":
-    unless => "mysql -u${name} -p${password} && test -f ~/.puppet_mysql_${name}_grants && grep -F \"${grant}\" ~/.puppet_mysql_${name}_grants",
-    command => "/usr/bin/mysql -uroot -p${mysql_password} -e \"grant ${grant} to ${name}@localhost identified by '${password}';\"",
-    require => Service["mysql"],
-    notify => Exec["mysql-${name}-grants"]
+define mysql::user($password, $priviledges) {
+  mysql::grant{ $priviledges: user => $name, password => $password }
+}
+
+define mysql::grant($user, $password) {
+  if ($name =~ /(?i:grant)/) {
+     $cmd = "/usr/bin/mysql -uroot -p${mysql_password} -e \"${name} to ${user}@localhost identified by '${password}';\""
+  } 
+
+  if ($name =~ /(?i:revoke)/) {
+     $cmd = "/usr/bin/mysql -uroot -p${mysql_password} -e \"${name} from ${user}@localhost;\""
   }
 
-  exec { "mysql-${name}-grants":
-    command => "echo \"${grant}\" > ~/.puppet_mysql_${name}_grants",
+  $priviledge_name = regsubst($name, ' ', '-', 'G')
+
+  exec { "set-${user}-${priviledge_name}":
+    unless => "mysql -u${user} -p${password} -e \"show grants;\" && test -f ~/.puppet_mysql_${user}_priviledges && grep -F \"${name}\" ~/.puppet_mysql_${user}_priviledges",
+    command => $cmd,
+    require => Service["mysql"],
+    notify => Exec["track-${user}-${priviledge_name}"]
+  }
+
+  exec { "track-${user}-${priviledge_name}":
+    command => "echo \"${name}\" >> ~/.puppet_mysql_${user}_priviledges",
     refreshonly => true
   }
 }
