21b4896d1b5f5e860582db8b8e32823d6fe17a66
Matteo Latini <mtylty@gmail.com>
2012-01-23 15:01:01 +0100

    updates rvm install script

--- rvm/manifests/classes/system.pp
+++ rvm/manifests/classes/system.pp
@@ -4,7 +4,7 @@ class rvm::system($version='latest') {
 
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm -o rvm-installer ; chmod +x rvm-installer ; rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man ./rvm-installer --version ${version}'",
+    command => "bash -s -- --version ${version} < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
       Class['rvm::dependencies'],
